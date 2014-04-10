ae6a1ff5872a9b2be4d25178abc50dd0920f0ec7
kurt <kurtlemond@gmail.com>
2014-04-10 17:38:55 +0800

    Bug Fix

--- GeomSolver.java
+++ GeomSolver.java
@@ -131,7 +131,7 @@ public class GeomSolver{
 		double length = input.nextDouble();
   
 		
-		System.out.println("Your square's area is " + length*43);
+		System.out.println("Your square's area is " + length*4);
 	}//getSquarePerimeter
 	
 	public static void getSquareArea(){
