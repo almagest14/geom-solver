d479512577f9bcc98ecf13fd35e3d364cd137c65
Venus <venus.arconada@gmail.com>
2014-04-10 19:50:52 +0800

    Modified Circle.java

--- Circle.java
+++ Circle.java
@@ -6,7 +6,7 @@ public class Circle{
 	public static double area = 0;
 	public static double diameter = 0;
 	
-	public static void main(String args[]){
+	public static void main(String[] args){
 			
 	}
 	
