3f3523b0884d56ab12702684315ae847337d9a93
Venus <venus.arconada@gmail.com>
2014-04-10 20:17:38 +0800

    Modified Circle.java

--- Circle.java
+++ Circle.java
@@ -1,31 +1,32 @@
 import java.util.*;
 
-public class Circle{
+public class geomsolver{
 
-	public static double perimeter=0;
+	public static double circumf=0;
 	public static double area = 0;
 	public static double diameter = 0;
 	
 	public static void main(String[] args){
-			
+		getCircleArea();	
 	}
 	
 	public static void getCircumference(){
 		
 		double pi = 3.1416;
 		Scanner in = new Scanner (System.in);
-		System.out.println("Computing for Circle's Area\n\n");
+		System.out.println("Computing for Circle's Circumference\n\n");
 		System.out.print("Enter diameter: ");
 		try {
 		diameter = in.nextDouble();
+		circumf	= pi*diameter;
+		System.out.println("The circle's area is: " + circumf);
 		}
 		
 		catch (InputMismatchException ime) {
-		System.out.println("Invalid input. Try Again.");
+		System.out.println("Invalid input.");
 		in.next();
 		}
-		perimeter	= pi*diameter;
-		System.out.println("The circle's area is: " + perimeter);
+		
 	}//getCircumference
 	
 	public static void getCircleArea(){
@@ -36,15 +37,15 @@ public class Circle{
 		System.out.print("Enter diameter: ");
 		try {
 		diameter = in.nextDouble();
+		area = pi*(Math.pow((diameter/2), 2));
+		System.out.println("The circle's area is: " + area);
 		}
 		catch(InputMismatchException ime) {
 		System.out.println("Invalid input. Try Again.");
 		in.next();
 		}
 		
-		area = pi*(Math.pow((diameter/2), 2));
-		System.out.println("Computing..");
-		System.out.println("The circle's area is: " + area);
+		
 	}//getCircleArea
 	
 	
