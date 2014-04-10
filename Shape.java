3f072503f5f8d8be0f628860b1efc3766773791b
John Mari D. Hinolan <hinolanjohn@yahoo.com>
2014-04-10 15:33:24 -0700

    Final with exception

--- Shape.java
+++ Shape.java
@@ -9,29 +9,34 @@ SideB = b;
 SideC = c;
 }
 double getArea(){
-double s = (SideA + SideB + SideC)/2;
+double s = (SideA + SideB + SideC)/2; // using Heron's Formula
 return Math.sqrt(s*(s-SideA)*(s-SideB)*(s-SideC));}
 double getPerimeter(){
 return (SideA + SideB + SideC);}
-}
-class TriangleDemo{DecimalFormat numberFormat = new DecimalFormat("#.00");
-Scanner input = new Scanner(System.in);
-void GetEverything(){
 
+}
+class TriangleDemo extends Exception {DecimalFormat numberFormat = new DecimalFormat("#.00");
+Scanner input = new Scanner(System.in);
+ void GetEverything(){
+try{double a = 0;
+double b = 0;
+double c =0;
 System.out.print("Enter Length of First Side :");
-double a = input.nextDouble();
+a = input.nextDouble();
 System.out.print("Enter Length of Second Side :");
-double b = input.nextDouble();
+ b = input.nextDouble();
 System.out.print("Enter Length of Third Side :");
-double c = input.nextDouble();
+ c = input.nextDouble();
 TriangleProp sample = new TriangleProp(a,b,c);
 System.out.println("Area of the Triangle : " + numberFormat.format(sample.getArea()));
-System.out.println("Perimeter of the Triangle : " + sample.getPerimeter());
+System.out.println("Perimeter of the Triangle : " + sample.getPerimeter());}
+catch(Exception e){
+ System.out.println("\nInput Error Please try again");
+ //GetEverything();
+ }
 }}
 public class Shape{
 public static void main(String [] args){
 TriangleDemo Demo = new TriangleDemo();
 Demo.GetEverything();
-
-
 }}
